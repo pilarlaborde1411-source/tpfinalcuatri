4be2fa93f6c920f4326614dbfebf075a94cefa34
Alejandra-00 <agomez@eest3necochea.edu.ar>
2026-04-30 16:58:53 -0300

    registarse
    🐷

--- registrarse.php
+++ registrarse.php
@@ -3,19 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="sesion.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="registrarse.css">
+    <title>Registrarse</title>
 </head>
 <body>
-    <div class="conteiner">
-        <div class="fotito"></div>
-        <form method="POST" action="registrarse.php">
+    <div class="container">
+        <div class="usuario"></div>
+        <form method="POST" action="registrarse.php" class="form">
             <input type="text" name="nombre" placeholder="Usuario" required>
             <input type="email" name="email" placeholder="Email" required>
             <input type="password" name="contraseña" placeholder="Contraseña" required>
-            <button type="submit">Crear cuenta</button>
-            <p>¿Ya tienes una cuenta?<a href="sesion.php">¡Inicia sesión!</a></p> <br> 
+            <button type="submit" class="btn btn-outline-dark">Crear cuenta</button>
         </form>
+        <p>¿Ya tienes una cuenta?<a href="sesion.php">¡Inicia sesión!</a></p>
     </div>
 </body>
 </html>
