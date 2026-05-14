48e0b17e80a399999a107ba72d98248e3b1a9253
Ale <agomez@eest3necochea.edu.ar>
2026-05-14 16:35:45 -0300

    Cambio en la fotito de mierda parte 2 <3
    Al menos ya tiene un tamaño decente.

--- index.php
+++ index.php
@@ -9,12 +9,15 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
+
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><h1>BUBBA STORE</h1></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+    <!--Menú-->
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
@@ -26,26 +29,32 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Ofertas</a>
         </li>
-        <li class="nav-item dropdown; justify-content: center;">
+        
+        <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Colecciones
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="top.php">Tops y Remeras</a></li>
+          <ul class="dropdown-menu ms-auto">
             <li><a class="dropdown-item" href="pantalon.php">Pantalones</a></li>
             <li><a class="dropdown-item" href="zapatos.php">Zapatos</a></li>
             <li><a class="dropdown-item" href="#">Camperas</a></li>
             <li><a class="dropdown-item" href="#">Joyas</a></li>
           </ul>
+        </li>
+
+      <!--Foto de perfil de usuario-->
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle; display: none;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <img id="img_usuario" src="im/usuario.png" alt="">
+            <img id="img_usuario" src="im/usuario.png" alt="Usuario" class="rounded-circle" width="40" height="40">
           </a>
-          <ul class="dropdown-menu">
+          <ul class="dropdown-menu dropdown-menu-end">
             <li><a class="dropdown-item" href="config.php">Configuración</a></li>
             <li><a class="dropdown-item" href="logout.php">Cerrar sesión</a></li>
           </ul>
         </li>
       </ul>
+    </ul>
     </div>
   </div>
 </nav>
