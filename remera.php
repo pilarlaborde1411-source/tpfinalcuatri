42c1750a80833580b8522b153bad858c3fc80beb
Ale <agomez@eest3necochea.edu.ar>
2026-05-14 17:27:19 -0300

    cambios
    Nuevo link en index.php, cambios en el nombre de top.php  a remera.php

--- remera.php
+++ remera.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Remeras y Tops</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="top.css">
+    <link rel="stylesheet" href="remera.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </head>
 <body>
