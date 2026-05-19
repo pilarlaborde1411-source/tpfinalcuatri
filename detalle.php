9c3c484f66cecd7af854554fcfe6c5525b636b6b
pilarlaborde1411-source <pilarlaborde1411@gmail.com>
2026-05-19 10:09:59 -0300

    Create detalle.php
    lo dice en el titulo

---
+++ detalle.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include ('conexion.php');
+    $sql = "SELECT * FROM producto";
+    $resultado = mysqli_query($conexion,$sql);
+
+    if (mysqli_num_rows ($resultado) > 0){
+        while($datos = mysqli_fetch_assoc($resultado)){
+            echo "<h1>" . $datos["nombre"]. "</h1>".  
+            "<img src'".$datos['imagen']."'>";
+        }
+    }
+    ?>
+</body>
+</html>
