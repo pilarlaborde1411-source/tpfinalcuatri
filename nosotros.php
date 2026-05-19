7a5d721c794dafbf8410b9fccbbf76f344603941
agnerodriguez <agnetha.rodriguez@eest3necochea.edu.ar>
2026-05-19 16:40:48 -0300

    nosotros
    archivos nososotros.php y nosostos.css

---
+++ nosotros.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="nosotros.css">
+    <title>Nosotros</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h1 >BUBBA STORE</h1></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nosotros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ofertas.php">Ofertas</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Colecciones
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="remera.php">Tops y Remeras</a></li>
+            <li><a class="dropdown-item" href="zapatos.php">Zapatos</a></li>
+            <li><a class="dropdown-item" href="camperas.php">Camperas</a></li>
+            <li><a class="dropdown-item" href="#">Joyas</a></li>
+          </ul>
+        </li>
+    </ul>
+</div>
+  </div>
+</nav>
+<h1 class= >Sobre Nosotros</h1>
+</body>
+</html>
