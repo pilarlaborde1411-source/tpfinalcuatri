ecbb60e39a40411814ea73bbf8e5e88a8e66b720
agnerodriguez <agnetha.rodriguez@eest3necochea.edu.ar>
2026-05-21 15:57:40 -0300

    Nosotros
    Datos del local y ubicación

--- nosotros.php
+++ nosotros.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Nosotros</a>
+          <a class="nav-link" href="n">Nosotros</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="ofertas.php">Ofertas</a>
@@ -40,6 +40,22 @@
 </div>
   </div>
 </nav>
-<h1 class= >Sobre Nosotros</h1>
+
+<h1 style="margin-top: 2%;">Sobre Nosotros</h1>
+<div style="display: flex; justify-content: space-between; align-items: flex-start; margin: 5% 10%;">
+    <div>
+        <h2 style="margin-top: 5%;">+54 001 234567</h2>
+        <h2 style="margin-top: 9%;">bubba.store@gmail.com</h2>
+        <h2 style="margin-top: 14%;">Te esperamos💖</h2>
+    </div>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63942.185115268985!2d-58.80060212668899!3d-38.57424133908916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x958f998e83b23fab%3A0xc4aad8746ef3c272!2sBubba%20storee!5e0!3m2!1ses-419!2sar!4v1779235898376!5m2!1ses-419!2sar"
+        width="600" height="450"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+</div>
 </body>
 </html>
