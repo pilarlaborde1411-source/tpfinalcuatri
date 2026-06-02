67f6455faf8137e0b5b99a23a4a06b47f7d3f76d
pilarlaborde1411-source <pilarlaborde1411@gmail.com>
2026-06-02 15:00:34 -0300

    Update categoria.php
    creo que ya esta

--- categoria.php
+++ categoria.php
@@ -18,7 +18,6 @@ if(mysqli_num_rows($resultado)>0){
             <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js' integrity='sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q' crossorigin='anonymous'></script>
         </head>
         <body>
-        
            <h1>".$productos['categoria']."</h1>
             <div class='col-md-4 mb-4'> <a href='detalle.php?id_producto=".$productos['id']."' style='text-decoration: none; color: black;'>
             <div class='card' style='width: 25rem;'>
@@ -26,8 +25,8 @@ if(mysqli_num_rows($resultado)>0){
                 <div class='card-body'>
                 <h5 class='card-title'>".$productos['nombre']."</h5>
                 <p class='card-text'>Jean Ancho Azul</p>
-                <h3>".$productos['precio']."</h3>
-                <button class='btnSumar btn btn-primary' onclick='comprar()'>Comprar</button>
+                <h3> $ ".$productos['precio']."</h3>
+                <button style= 'margin-left: 30% ; width: 100px; font-family: Simple Dreams; border-radius: 15px;' class='btnSumar btn btn-primary' onclick='comprar()'>Comprar</button>
                 </div>
             </div>
             </div>
