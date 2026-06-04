4ad62d7fdcee5f73b27113fbbae0f1a2cdd3d12a
agnerodriguez <agnetha.rodriguez@eest3necochea.edu.ar>
2026-06-04 17:31:24 -0300

    nosotros
    agregue estilo al nosotros y NO rompi la pagina esta vez

--- nosotros.php
+++ nosotros.php
@@ -3,72 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="nosotros.css">
-    <title>Nosotros</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><h1 >BUBBA STORE</h1></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Nosotros</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="ofertas.php">Ofertas</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Colecciones
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="remera.php">Tops y Remeras</a></li>
-            <li><a class="dropdown-item" href="zapatos.php">Zapatos</a></li>
-            <li><a class="dropdown-item" href="camperas.php">Camperas</a></li>
-            <li><a class="dropdown-item" href="#">Joyas</a></li>
-          </ul>
-        </li>
-    </ul>
-</div>
-  </div>
-</nav>
+<?php include 'navegacion.php'?>
 
-<h1 style="margin-top: 2%;">Sobre Nosotros</h1>
-<div style="display: flex; justify-content: space-between; align-items: flex-start; margin: 5% 10%;">
+<h1 style="margin: 2% auto 0; text-align: center;">Sobre Nosotros</h1>
+<div style="display: flex; justify-content: space-between; align-items: flex-start; margin: 2% 10%;">
     <div>
-        <h2 style="margin-top: 5%;">+54 001 234567</h2>
-        <h2 style="margin-top: 15%;">bubba.store@gmail.com</h2>
-        <h2 style="margin-top: 22%;">Te esperamos💖</h2>
+        <h2 style="margin-top:8%;">
+          <img src="im/iconos/whatsapp.png" alt="" style="width: 9%;">
+          +𝟱𝟰 𝟬𝟬𝟭 𝟮𝟯𝟰𝟱𝟲𝟳</h2>
+
+        <h2 style="margin-top: 10%;">
+          <img src="im/iconos/gmail.png" alt="" style="width: 9%;">
+          𝙗𝙪𝙗𝙗𝙖.𝙨𝙩𝙤𝙧𝙚@𝙜𝙢𝙖𝙞𝙡.𝙘𝙤𝙢</h2>
+
+        <h2 style="margin-top: 12%;">
+          𝙏𝙚 𝙚𝙨𝙥𝙚𝙧𝙖𝙢𝙤𝙨<img src="im/iconos/coraozn.png" alt="" style="width: 6%;"></h2>
     </div>
-    <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63942.185115268985!2d-58.80060212668899!3d-38.57424133908916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x958f998e83b23fab%3A0xc4aad8746ef3c272!2sBubba%20storee!5e0!3m2!1ses-419!2sar!4v1779235898376!5m2!1ses-419!2sar"
-        width="600" height="450"
-        style="border:0;"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade">
-    </iframe>
+    
+    <div class="col-lg-7">
+      <div class="card shadow border-0 rounded-4 overflow-hidden">
+          <h5>📍 Calle 85 entre 4 y 2</h5>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63942.185115268985!2d-58.80060212668899!3d-38.57424133908916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x958f998e83b23fab%3A0xc4aad8746ef3c272!2sBubba%20storee!5e0!3m2!1ses-419!2sar!4v1779235898376!5m2!1ses-419!2sar"
+          width="100%" height="450" style="border:0;" loading="lazy">
+        </iframe>
+      </div>
+    </div>
 </div>
-<footer class="pie"> 
-  <footer>
-        <a href="#navbarNavDropdown"><p class="Volver">Volver arriba</P></a>
-        <hr>
-        <div class="foot" id="redes">
-            <a href=""><div id="pinterest" class="logo"></div></a>
-            <a href=""><div id="twitter"  class="logo"></div></a>
-            <a href=""><div id="instagram" class="logo"></div>
-            <a href=""><div id="youtube" class="logo"></div></a>
-            <a href=""><div id="facebook" class="logo"></div></a>
-        </div>
-    </footer>
-</footer>
+
+<?php include 'footer.php'?>
 </body>
 </html>
