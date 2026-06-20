c6f318ca889ef4eb46a60f04f3d73d65fbc1bcad
agnerodriguez <agnetha.rodriguez@eest3necochea.edu.ar>
2026-06-19 22:49:36 -0300

    carrito
    no se reptien los productos en el carrito :D

--- agregarCarrito.php
+++ agregarCarrito.php
@@ -5,9 +5,17 @@
     $idProducto = $_GET['id'];  //toma la id del producto
     $idUsuario = $_SESSION['id_usuario']; //toma la id del usuario
 
-    $sql = "INSERT INTO carrito(`id-usuario`, `id-producto`, cantidad) VALUES ('$idUsuario', '$idProducto', 1)"; //guarda el producto en la tabla carrito
+    $contenidoCarrito = "SELECT * FROM carrito WHERE `id-usuario` = '$idUsuario' AND `id-producto` = '$idProducto'";
+    $resultado = $conexion->query($contenidoCarrito);
 
+    if($resultado->num_rows > 0){ //history.back vuelve a la pagina de donde vino el usuario
+        echo " <script> alert('El producto ya se agrego al carrito'); 
+        history.back();
+        </script>";
+    } else {
+        $sql = "INSERT INTO carrito(`id-usuario`, `id-producto`, cantidad) VALUES ('$idUsuario', '$idProducto', 1)"; //guarda el producto en la tabla carrito 
     $conexion->query($sql);
     header("Location: index.php");
     exit;
+    }
 ?>
